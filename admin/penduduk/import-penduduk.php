f929098eb747191fc272ee29f386f39c2ee5ff60
nVITTOadrianto <nvittoa@live.com>
2024-06-03 18:06:14 +0700

    php excel reader diganti dengan PhpSpreadsheet, dan import penduduk sekarang bekerja

--- admin/penduduk/import-penduduk.php
+++ admin/penduduk/import-penduduk.php
@@ -1,43 +1,43 @@
 <?php 
 	include ('../../config/koneksi.php');
-	include ('excel_reader2.php');
+	include ('../../assets/vendor/autoload.php');
 
-	$target = basename($_FILES['datapenduduk']['name']) ;
-	move_uploaded_file($_FILES['datapenduduk']['tmp_name'], $target);
-	chmod($_FILES['datapenduduk']['name'],0777);
-	$data = new Spreadsheet_Excel_Reader($_FILES['datapenduduk']['name'],false);
-	$jumlah_baris = $data->rowcount($sheet_index=0);
-	$berhasil = 0;
-	for ($i=2; $i<=$jumlah_baris; $i++){
-		$nik  = $data->val($i, 1);
-		$nama  = $data->val($i, 2);
-		$tempat_lahir  = $data->val($i, 3);
-		$tgl_lahir  = $data->val($i, 4);
-		$jenis_kelamin  = $data->val($i, 5);
-		$agama  = $data->val($i, 6);
-		$jalan  = addslashes($data->val($i, 7));
-		$dusun  = $data->val($i, 8);
-		$rt  = $data->val($i, 9);
-		$rw  = $data->val($i, 10);
-		$desa  = $data->val($i, 11);
-		$kecamatan  = $data->val($i, 12);
-		$kota  = $data->val($i, 13);
-		$no_kk  = $data->val($i, 14);
-		$pend_kk  = $data->val($i, 15);
-		$pend_terakhir  = $data->val($i, 16);
-		$pend_ditempuh  = $data->val($i, 17);
-		$pekerjaan  = $data->val($i, 18);
-		$status_perkawinan  = $data->val($i, 19);
-		$status_dlm_keluarga  = $data->val($i, 20);
-		$kewarganegaraan  = $data->val($i, 21);
-		$nama_ayah  = $data->val($i, 22);
-		$nama_ibu  = $data->val($i, 23);
-		
-		if($nik != "" && $nama != "" && $tempat_lahir != "" && $tgl_lahir != "" && $jenis_kelamin != "" && $agama != "" && $jalan != "" && $dusun != "" && $rt != "" && $rw != "" && $desa != "" && $kecamatan != "" && $kota != "" && $no_kk != "" && $pend_kk != "" && $pend_terakhir != "" && $pend_ditempuh != "" && $pekerjaan != "" && $status_perkawinan != "" && $status_dlm_keluarga != "" && $kewarganegaraan != "" && $nama_ayah != "" && $nama_ibu != ""){
-			mysqli_query($connect,"INSERT INTO penduduk VALUES(NULL, '$nik','$nama','$tempat_lahir','$tgl_lahir','$jenis_kelamin','$agama','$jalan','$dusun','$rt','$rw','$desa','$kecamatan','$kota','$no_kk','$pend_kk','$pend_terakhir','$pend_ditempuh','$pekerjaan','$status_perkawinan','$status_dlm_keluarga','$kewarganegaraan','$nama_ayah','$nama_ibu')");
-			$berhasil++;
+	if(isset($_POST['upload'])) {
+		$target_name = $_FILES['datapenduduk']['name'];
+		$target_data = $_FILES['datapenduduk']['tmp_name'];
+
+		$reader = PhpOffice\PhpSpreadsheet\IOFactory::createReaderForFile($target_data);
+		$imported_data = $reader->load($target_data);
+		$data = $imported_data->getActiveSheet()->toArray();
+		for($i = 1; $i < count($data); $i++) {
+			$nik  = $data[$i]['0'];
+			$nama  = $data[$i]['1'];
+			$tempat_lahir  = $data[$i]['2'];
+			$tgl_lahir  = $data[$i]['3'];
+			$jenis_kelamin  = $data[$i]['4'];
+			$agama  = $data[$i]['5'];
+			$jalan  = addslashes($data[$i]['6']);
+			$dusun  = $data[$i]['7'];
+			$rt  = $data[$i]['8'];
+			$rw  = $data[$i]['9'];
+			$desa  = $data[$i]['10'];
+			$kecamatan  = $data[$i]['11'];
+			$kota  = $data[$i]['12'];
+			$no_kk  = $data[$i]['13'];
+			$pend_kk  = $data[$i]['14'];
+			$pend_terakhir  = $data[$i]['15'];
+			$pend_ditempuh  = $data[$i]['16'];
+			$pekerjaan  = $data[$i]['17'];
+			$status_perkawinan  = $data[$i]['18'];
+			$status_dlm_keluarga  = $data[$i]['19'];
+			$kewarganegaraan  = $data[$i]['20'];
+			$nama_ayah  = $data[$i]['21'];
+			$nama_ibu  = $data[$i]['21'];
+
+			if($nik != "" && $nama != "" && $tempat_lahir != "" && $tgl_lahir != "" && $jenis_kelamin != "" && $agama != "" && $jalan != "" && $dusun != "" && $rt != "" && $rw != "" && $desa != "" && $kecamatan != "" && $kota != "" && $no_kk != "" && $pend_kk != "" && $pend_terakhir != "" && $pend_ditempuh != "" && $pekerjaan != "" && $status_perkawinan != "" && $status_dlm_keluarga != "" && $kewarganegaraan != "" && $nama_ayah != "" && $nama_ibu != ""){
+				mysqli_query($connect,"INSERT INTO penduduk VALUES(NULL, '$nik','$nama','$tempat_lahir','$tgl_lahir','$jenis_kelamin','$agama','$jalan','$dusun','$rt','$rw','$desa','$kecamatan','$kota','$no_kk','$pend_kk','$pend_terakhir','$pend_ditempuh','$pekerjaan','$status_perkawinan','$status_dlm_keluarga','$kewarganegaraan','$nama_ayah','$nama_ibu')");
+			}
 		}
 	}
-	unlink($_FILES['datapenduduk']['name']);
 	header("location:index.php");
 ?>
